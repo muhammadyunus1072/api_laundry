04d3deef4e77c2aaefb2a7fd324a5109d5b8f3ab
muhammadyunus1072 <muhammadyunus1072@gmail.com>
2022-07-01 14:52:05 +0700

    configurasi APP_URL laravel

--- config/app.php
+++ config/app.php
@@ -54,7 +54,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://friendly-smarties-82988.herokuapp.com/'),
 
     'asset_url' => env('ASSET_URL'),
 
